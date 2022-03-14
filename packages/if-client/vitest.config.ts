9ef06f95a004db40a32bf1bb64a8d0a4986904ba
Gabriel R. Antunes <gabrielrodantunes@gmail.com>
2022-03-14 19:51:17 -0400

    feat(packages/if-client): init

---
+++ packages/if-client/vitest.config.ts
@@ -0,0 +1,13 @@
+/// <reference types="vitest" />
+import { defineConfig } from "vitest/config";
+
+// https://vitest.dev/config/
+export default defineConfig({
+  test: {
+    coverage: {
+      all: true,
+      reporter: ["text"],
+      include: ["src"],
+    },
+  },
+});
